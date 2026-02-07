7121e6b39db94c7aaa4ce9af81be88438eb1d815
muhammedamirshaaslam3333-spec <muhammedamirshaaslam3333@gmail.com>
2026-02-07 13:25:25 +0530

    initial commit"

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrollToSection() {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+// Optional: simple form submit message
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+    form.reset();
+});
