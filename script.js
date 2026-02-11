4ff7d4c4e9d86b3dee45b1b5a1d0824d525990af
muhammedamirshaaslam3333-spec <muhammedamirshaaslam3333@gmail.com>
2026-02-11 11:46:52 +0530

    updated ui

--- script.js
+++ script.js
@@ -12,5 +12,15 @@ form.addEventListener("submit", function (e) {
     alert("Thank you! Your message has been sent.");
     form.reset();
 });
+const pic = document.querySelector(".profile-pic");
+
+document.addEventListener("mousemove", (e) => {
+    const x = (window.innerWidth / 2 - e.clientX) / 25;
+    const y = (window.innerHeight / 2 - e.clientY) / 25;
+
+    pic.style.transform = `translate(${-x}px, ${-y}px)`;
+});
+
+
 
 
