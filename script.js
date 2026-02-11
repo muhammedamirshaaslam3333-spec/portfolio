152faaf806189c328a8a888099475de54c856f6a
muhammedamirshaaslam3333-spec <muhammedamirshaaslam3333@gmail.com>
2026-02-11 16:01:22 +0530

    update ui

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ form.addEventListener("submit", function (e) {
     alert("Thank you! Your message has been sent.");
     form.reset();
 });
-const pic = document.querySelector(".profile-pic");
+const pic = document.querySelector("h1");
 
 document.addEventListener("mousemove", (e) => {
     const x = (window.innerWidth / 2 - e.clientX) / 25;
